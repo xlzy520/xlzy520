63329dc50ff3fae7612a931b7baed1032e3059f9
zhibi <1017866168@qq.com>
2019-10-30 22:35:07 +0800

    update 使用externals和cdn优化

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,13 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? `https://cdn.jsdelivr.net/gh/xlzy520/xlzy520.github.io/` : '/',
   chainWebpack(config) {
     const cdn = {
-      css: ['https://fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC&display=swap'],
+      css: [
+        'https://fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC&display=swap',
+      ],
       js: [
         'https://cdn.bootcss.com/jquery/3.4.0/jquery.min.js',
-        'https://cdn.bootcss.com/jquery-backstretch/2.0.4/jquery.backstretch.min.js'
+        'https://cdn.bootcss.com/jquery-backstretch/2.0.4/jquery.backstretch.min.js',
+        'https://cdn.bootcss.com/gitalk/1.5.0/gitalk.min.js',
       ]
     }
     config.plugin('html').tap(args => {
@@ -22,6 +25,11 @@ module.exports = {
           @import "~@/styles/mixin.scss";
         `
       }
+    }
+  },
+  configureWebpack: {
+    externals:{
+      gitalk: 'gitalk',
     }
   }
 }

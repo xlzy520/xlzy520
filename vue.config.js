28db5a94a9cfd5e25bd863d368e830897752ce1b
zhibi <1017866168@qq.com>
2019-10-30 17:37:46 +0800

    update 修改publicPath和cdn地址

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? `//cdn.jsdelivr.net/gh/xlzy520/xlzy520.github.io/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `https://cdn.jsdelivr.net/gh/xlzy520/xlzy520.github.io/` : '/',
   chainWebpack(config) {
     const cdn = {
-      css: ['//fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC&display=swap'],
+      css: ['https://fonts.googleapis.com/css?family=Fira+Mono|Noto+Serif+SC&display=swap'],
       js: [
         'https://cdn.bootcss.com/jquery/3.4.0/jquery.min.js',
         'https://cdn.bootcss.com/jquery-backstretch/2.0.4/jquery.backstretch.min.js'

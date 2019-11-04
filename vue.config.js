8f602335c1b3a0500aeba55bab4bae13abf39e66
zhibi <1017866168@qq.com>
2019-11-04 16:17:58 +0800

    update 优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+const externals = ()=>{
+  if (isProd) {
+    return {
+      'gitalk': 'Gitalk',
+      'leancloud-storage': 'AV'
+    }
+  }
+  return {}
+}
 module.exports = {
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ? `https://cdn.jsdelivr.net/gh/xlzy520/xlzy520.github.io/` : '/',
@@ -15,12 +25,16 @@ module.exports = {
         'https://cdn.bootcss.com/jquery-backstretch/2.0.4/jquery.backstretch.min.js',
       ]
     }
+    if (isProd) {
+      cdn.js.concat([
+        'https://cdn.bootcss.com/gitalk/1.5.0/gitalk.min.js',
+        'https://cdn1.lncld.net/static/js/3.0.4/av-min.js'
+      ])
+    }
     config.plugin('html').tap(args => {
       args[0].cdn = cdn
       return args
     })
-    config.resolve.alias
-      .set('@', resolve('src'));
   },
   css: {
     loaderOptions: {
@@ -32,6 +46,7 @@ module.exports = {
       }
     }
   },
-  configureWebpack: {
+  configureWebpack: config=>{
+    config.externals = externals()
   }
 }

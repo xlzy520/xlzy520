a6a5c6c603f19c8f0e724e04001f7e1a55db1e1b
zhibi <1017866168@qq.com>
2019-11-04 11:02:18 +0800

    update hl高亮优化

--- src/assets/lib/highlight.js
+++ src/assets/lib/highlight.js
@@ -1,11 +1,12 @@
 import hljs from 'highlight.js/lib/highlight'
 import javascript from 'highlight.js/lib/languages/javascript'
+import typescript from 'highlight.js/lib/languages/typescript'
 import xml from 'highlight.js/lib/languages/xml'
-import less from 'highlight.js/lib/languages/less'
+import scss from 'highlight.js/lib/languages/scss'
 import css from 'highlight.js/lib/languages/css'
 import java from 'highlight.js/lib/languages/java'
 import python from 'highlight.js/lib/languages/python'
-import objectivec from 'highlight.js/lib/languages/objectivec'
+import shell from 'highlight.js/lib/languages/shell'
 import markdown from 'highlight.js/lib/languages/markdown'
 import bash from 'highlight.js/lib/languages/bash'
 import json from 'highlight.js/lib/languages/json'
@@ -19,15 +20,15 @@ linenumber(hljs)
 // 按需导入高亮语种
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('xml', xml)
-hljs.registerLanguage('less', less)
+hljs.registerLanguage('scss', scss)
 hljs.registerLanguage('css', css)
 hljs.registerLanguage('java', java)
 hljs.registerLanguage('python', python)
-hljs.registerLanguage('objectivec', objectivec)
 hljs.registerLanguage('markdown', markdown)
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('json', json)
-hljs.registerLanguage('http', http)
+hljs.registerLanguage('typescript', typescript)
+hljs.registerLanguage('shell', shell)
 
 hljs.initHighlightingOnLoad()
 

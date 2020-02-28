36e3fc290e5edef95fd9581a875aaf9aa723e908
chanshiyu <me@chanshiyu.com>
2020-02-28 11:58:19 +0800

    fix: 修复eslint规则
    (cherry picked from commit 504b72fad3309895af6e1af933c793f6f9df1506)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential'],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {

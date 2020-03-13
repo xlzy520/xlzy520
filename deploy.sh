dc77b90bfbfee53e4a99c82cab2cac1935197c7c
xlzy <1017866168@qq.com>
2020-03-13 15:53:35 +0800

    add: 增加GitHub actions ci部署,增加百度收录

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,27 @@ npm run build
 cd dist
 
 # 部署到自定义域名，没有自定义域名可注释掉
-# echo 'xlzy520.cn' > CNAME
+echo 'xlzy520.cn' > CNAME
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xlzy520/xlzy520.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://xlzy520:${GITHUB_TOKEN}@github.com/xlzy520/xlzy520.github.io.git
+  git config --global user.name "xlzy"
+  git config --global user.email "1017866168@qq.com"
+fi
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'github actions deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:xlzy520/xlzy520.github.io.git master
+git push -f $githubUrl master
+#git push -f git@github.com:xlzy520/xlzy520.github.io.git master
 
 # 删除dist文件夹的.git文件夹
-rm .git
+#rm .git
 
 cd -
